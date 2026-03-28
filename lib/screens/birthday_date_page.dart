f9b50f5463934cf6db2005f5c6fcdc750e1ad13b
kii <ardhikaz30@gmail.com>
2026-03-28 22:54:38 +0700

    birthdate

---
+++ lib/screens/birthday_date_page.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class BirthdayDatePage extends StatefulWidget {
+  const BirthdayDatePage({super.key});
+
+  @override
+  State<BirthdayDatePage> createState() => _BirthdayDatePageState();
+}
+
+class _BirthdayDatePageState extends State<BirthdayDatePage> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
